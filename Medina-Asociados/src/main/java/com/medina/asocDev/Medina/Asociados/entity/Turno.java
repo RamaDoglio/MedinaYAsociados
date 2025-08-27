26f24d4b59c001deae54a61b0557713e0e790381
Manuel <manuelveronese7@gmail.com>
2025-08-27 16:17:11 -0300

    turno

--- Medina-Asociados/src/main/java/com/medina/asocDev/Medina/Asociados/entity/Turno.java
+++ Medina-Asociados/src/main/java/com/medina/asocDev/Medina/Asociados/entity/Turno.java
@@ -1,4 +1,17 @@
 package com.medina.asocDev.Medina.Asociados.entity;
 
+@Data
+@Entity
+@Table(name= "Turnos")
 public class Turno {
+    @Id
+    @GenerateValue(strategy = GenerationType.IDENTITY)
+    private Long idTurno;
+
+    private estadoActual
+    private LocalDateTime fechaHoraInicio;
+    private LocalDateTime fechaHoraFin;
+
+    private String observaciones;
+
 }
